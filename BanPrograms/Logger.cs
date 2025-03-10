1004222b126b4640d751487342021fe0b94363a0
Никита Коровкин <twodrops@mail.ru>
2025-03-10 15:53:36 +0700

    added Logger.cs

---
+++ BanPrograms/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanPrograms
+{
+    public class Logger
+    {
+        private const string LogFile = "attempts.log";
+
+        public void Log(string message)
+        {
+            string entry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}";
+            File.AppendAllText(LogFile, entry + Environment.NewLine);
+        }
+    }
+    
+}
