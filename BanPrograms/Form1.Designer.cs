ba610da79f3f99e573fe5a40c8b58704531fc986
Никита Коровкин <twodrops@mail.ru>
2025-03-10 16:15:17 +0700

    add Form with List of programs, buttons: Remove, Add, Toggle

--- BanPrograms/Form1.Designer.cs
+++ BanPrograms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BanPrograms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.lstPrograms = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(513, 174);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(132, 66);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(513, 69);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(140, 75);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Location = new System.Drawing.Point(255, 390);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(242, 85);
+            this.btnToggle.TabIndex = 2;
+            this.btnToggle.Text = "Toggle";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // lstPrograms
+            // 
+            this.lstPrograms.FormattingEnabled = true;
+            this.lstPrograms.ItemHeight = 25;
+            this.lstPrograms.Location = new System.Drawing.Point(57, 49);
+            this.lstPrograms.Name = "lstPrograms";
+            this.lstPrograms.Size = new System.Drawing.Size(267, 154);
+            this.lstPrograms.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(719, 561);
+            this.Controls.Add(this.lstPrograms);
+            this.Controls.Add(this.btnToggle);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.ListBox lstPrograms;
     }
 }
 
--- BanPrograms/Form1.cs
+++ BanPrograms/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,79 @@ using System.Windows.Forms;
 
 namespace BanPrograms
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private ProgramListManager manager = new ProgramListManager();
+        private MonitorService monitor = new MonitorService();
+
+        public MainForm()
         {
-            InitializeComponent();
+            InitializeComponent(); // Этот метод сгенерирован дизайнером
+            CheckAdminRights();
+            LoadProgramList();
+        }
+
+        private void CheckAdminRights()
+        {
+            if (!manager.IsAdmin())
+            {
+                MessageBox.Show("Требуются права администратора!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnAdd.Enabled = btnRemove.Enabled = btnToggle.Enabled = false;
+            }
+        }
+
+        private void LoadProgramList()
+        {
+            var list = manager.LoadList();
+            lstPrograms.Items.Clear();
+            foreach (var program in list.Programs)
+            {
+                lstPrograms.Items.Add($"{program.Name} ({program.Path})");
+            }
+            btnToggle.Text = list.Enabled ? "Выключить" : "Включить";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var list = manager.LoadList();
+                    string hash = manager.CalculateHash(ofd.FileName);
+                    list.Programs.Add(new ProgramInfo
+                    {
+                        Name = Path.GetFileName(ofd.FileName),
+                        Path = ofd.FileName,
+                        Hash = hash
+                    });
+                    manager.SaveList(list);
+                    monitor.UpdateCache();
+                    LoadProgramList();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstPrograms.SelectedIndex >= 0)
+            {
+                var list = manager.LoadList();
+                list.Programs.RemoveAt(lstPrograms.SelectedIndex);
+                manager.SaveList(list);
+                monitor.UpdateCache();
+                LoadProgramList();
+            }
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            var list = manager.LoadList();
+            list.Enabled = !list.Enabled;
+            manager.SaveList(list);
+            monitor.UpdateCache();
+            if (list.Enabled) monitor.Start(); else monitor.Stop();
+            LoadProgramList();
         }
     }
 }
